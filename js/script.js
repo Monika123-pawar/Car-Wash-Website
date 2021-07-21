69c806da7e7420293c643b5789fdd6508689bf28
Monika Pawar <monika.pawar.we@gmail.com>
2021-07-21 17:53:36 +0530

    submit

--- js/script.js
+++ js/script.js
@@ -8,4 +8,21 @@ $(document).ready(function(){
       });
       $('.carousel').BannerSlide(opts);
 
+      $('.single-item').slick();
+
   });
+  var opts = {
+
+    // CSS class for 'next' arrow
+    arrowRight : '.arrow-right',
+  
+    // CSS class for 'prev' arrow
+    arrowLeft : '.arrow-left',
+  
+    // <a href="https://www.jqueryscript.net/animation/">Animation</a> speed
+    speed : 700,
+  
+    // Auto-play delay time
+    slideDuration : 4000
+    
+  };
